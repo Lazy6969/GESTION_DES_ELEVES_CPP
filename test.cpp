fe6b410822cdd3f2ee94f472385b0df4a105eedc
LAZY'6969 <mirindraramanana2@gmail.com>
2026-03-09 21:17:58 +0300

    test

---
+++ test.cpp
@@ -0,0 +1,29 @@
+//ajout des bibliothèques BASE
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+//Effacer les interfaces après chaque ouverture
+void cls(){
+    system("cls");
+}
+
+//Juste affichage du menu
+void menu(){
+    cls();
+    cout << "VOICI LE MENU :" << endl;
+    cout << "   1. Ajouter un étudiant " << endl;
+    cout << "   2. Rechercher un étudiant  " << endl;
+    cout << "   3. Listes des étudiants  " << endl;
+    cout << "   4. QUITTER " << endl;
+}
+
+//Fonction principale ou le programme execute
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
