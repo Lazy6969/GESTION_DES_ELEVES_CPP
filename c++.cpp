30e135b3050d7eafbd57046b0e3c79ac646848d1
LAZY'Coder <mirindraramanana2@gmail.com>
2026-03-09 22:50:20 +0300

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <fstream>
 using namespace std;
+
+void cls(){
+    system("cls");
+}
+
+void menu(){
+    cls();
+    cout << "VOICI LE MENU :" << endl;
+    cout << "   1. Ajouter un étudiant " << endl;
+    cout << "   2. Rechercher un étudiant  " << endl;
+    cout << "   3. Listes des étudiants  " << endl;
+    cout << "   4. QUITTER " << endl;
+}
 
 int main()
 {
     cout << "Hello world!" << endl;
     return 0;
 }
+
